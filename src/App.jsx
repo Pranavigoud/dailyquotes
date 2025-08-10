90daf6547730b95852224c5b960022af434cef7d
Pranavigoud <sathuripranavigoud12@gmail.com>
2025-08-10 20:40:46 +0530

    v 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
   }
   // fetchQuote()
   return (
-    <div className='bg-neutral-200 h-[100vh] flex justify-center items-center'>
+    <div className=' h-[100vh] flex justify-center items-center bg-[url("https://t4.ftcdn.net/jpg/03/98/29/37/360_F_398293720_XOwzFWNaEG23Nm7ipWeKKWnwB7f5vImI.jpg")] bg-no-repeat bg-cover'>
      <div className='flex flex-col text-center p-5 w-xl min-h-[30vh] h-fit bg-white rounded-4xl shadow-2xl max-lg:w-11/12'>
       <h1 className='flex justify-between text-2xl font-bold px-3'> <span className='text-neutral-800'>Daily Quotes❤️</span> <button onClick={()=>window.location.reload()} className='cursor-pointer'><FaArrowAltCircleRight/></button></h1>
       <h6 className='font-bold text-2xl text-neutral-700 pt-5'>{motive?.quote}</h6>
